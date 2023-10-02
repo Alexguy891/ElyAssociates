cd004fb005023702de45960c1db66120668e6259
Shihab Al Jahwari <alshohb@gmail.com>
2023-10-01 21:51:01 -0400

    Removed unwanted CSS and HTML files; Integrated styles into PHP files

--- website/failure.php
+++ website/failure.php
@@ -4,16 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Submission Failure</title>
-    <link rel="stylesheet" type="text/css" href="failure.css">
     <style>
         body {
             font-family: Arial, sans-serif;
+            background-color: #ffe6e6;  /* Light red background */
+            color: #8B0000;  /* Dark Red Text */
             text-align: center;
-            padding: 50px;
         }
+
+        h1 {
+            font-size: 2.5rem;
+            margin-bottom: 1rem;
+        }
+
+        p {
+            font-size: 1.2rem;
+            margin-bottom: 1.5rem;
+        }
+
         button {
-            padding: 10px 15px;
-            margin: 10px;
+            background-color: #8B0000;  /* Dark red */
+            color: white;
+            padding: 15px 30px;
+            border: none;
+            border-radius: 5px;
+            font-size: 1rem;
             cursor: pointer;
         }
     </style>
--- website/success.php
+++ website/success.php
@@ -4,16 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Submission Successful</title>
-    <link rel="stylesheet" type="text/css" href="success.css">
     <style>
         body {
             font-family: Arial, sans-serif;
+            background-color: #e6ffe6;  /* Light green background */
+            color: #006400;  /* Dark Green Text */
             text-align: center;
-            padding: 50px;
         }
+
+        h1 {
+            font-size: 2.5rem;
+            margin-bottom: 1rem;
+        }
+
+        p {
+            font-size: 1.2rem;
+            margin-bottom: 1.5rem;
+        }
+
         button {
-            padding: 10px 15px;
-            margin: 10px;
+            background-color: #006400;  /* Dark green */
+            color: white;
+            padding: 15px 30px;
+            border: none;
+            border-radius: 5px;
+            font-size: 1rem;
             cursor: pointer;
         }
     </style>
